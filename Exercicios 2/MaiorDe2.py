c49c0855fc425468952ec0a36cbeb7dd3e957057
Guilherme <guilherme.scarpioni@live.com>
2023-03-17 11:08:29 -0300

    alterações

---
+++ Exercicios 2/MaiorDe2.py
@@ -0,0 +1,9 @@
+import math
+
+n1 = int(input("Digite o primeiro numero:"))
+n2 = int(input("Digite o segundo numero"))
+
+if n1 > n2:
+    print("O primeiro numero é o maior")
+else:
+    print("O segundo numero é o maior")
