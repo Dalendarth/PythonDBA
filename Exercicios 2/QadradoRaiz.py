b71a1ea9e0025c571b56bb6651b38a8224f2675b
Guilherme <guilherme.scarpioni@live.com>
2023-03-17 15:54:07 -0300

    TesteReconhecimentoVoz

---
+++ Exercicios 2/QadradoRaiz.py
@@ -0,0 +1,11 @@
+import math
+
+n1 = float(input("Digite um Numero: "))
+
+if n1 >= 0:
+    quad = n1 ** 2
+    raizq = math.sqrt(n1)
+    print(f"O {n1} elevado ao quadrado fica {quad} e a raiz de {n1} é de {raizq} ")
+else:
+    n1 < 0
+    print("Numero Invalido")
