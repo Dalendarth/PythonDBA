1d3298024686cc6cad7d446967ce7268d83b313a
Guilherme <guilherme.scarpioni@live.com>
2023-03-17 12:44:05 -0300

    alterações

---
+++ Exercicios 2/TesteCondicional.py
@@ -0,0 +1,14 @@
+import math
+num = float(input("Digite um número real: "))
+
+if num >= 0:
+    pos = True
+else:
+    pos = False
+
+if pos:
+    raizq = math.sqrt(num)
+    print("A raiz quadrada de", num, "é:", raizq)
+else:
+    quadrado = num ** 2
+    print("O quadrado de", num, "é:", quadrado)
