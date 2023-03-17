1d131961005bbc426b9d396291bd842f52ef9252
Guilherme <guilherme.scarpioni@live.com>
2023-03-17 16:24:59 -0300

    Exercicio

---
+++ Exercicios 2/NumeroParOrImpar.py
@@ -0,0 +1,8 @@
+import math
+
+n = int(input("Digite um numero:"))
+
+if n % 2 == 0: #caso a parte, operador modulo "%" faz a divisão em 2 pra saber se sobra 1 (impar) apenas esse caso!
+    print("O numero é par.")
+else:
+    print("Onumero é impar.")
