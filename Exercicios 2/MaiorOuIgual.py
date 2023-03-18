8118695844a07529513dbcc1bdd9bacc47dcb834
Guilherme <guilherme.scarpioni@live.com>
2023-03-17 23:53:18 -0300

    MaiorIgual

---
+++ Exercicios 2/MaiorOuIgual.py
@@ -0,0 +1,11 @@
+import math
+
+n1 = int(input("Digite o primeiro numero:"))
+n2 = int(input("Digite o segundo numero"))
+
+if n1 > n2:
+    print("O primeiro numero é o maior")
+elif n1 == n2:
+    print("Numeros iguais")
+else:
+    print("O segundo numero é o maior")
