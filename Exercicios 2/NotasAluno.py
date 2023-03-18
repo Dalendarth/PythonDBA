0b40b4cbdbbbb947edc287279f464ba6d063f42e
Guilherme <guilherme.scarpioni@live.com>
2023-03-18 11:02:31 -0300

    ExercicioNotasCustomizado

--- Exercicios 2/NotasAluno.py
+++ Exercicios 2/NotasAluno.py
@@ -1,25 +1,9 @@
-n1 = float(input("Digite a primeira nota do aluno: "))
-n2 = float(input("Digite a segunda nota do aluno: "))
-
-if n1 > 0 and n2 > 0:
-    print(f"As notas digitadas foram: {n1} e {n2}, correto?")
-    while True:
-        s = input("Deseja calcular a média? (S/N) ")
-        if s.upper() == "S":
-            m = (n1 + n2) / 2
-            print(f"A média das notas é: {m}")
-            break
-        elif s.upper() == "N":
-            print("Encerrando programa...")
-            break
-        else:
-            print("Opção inválida. Digite S para calcular a média ou N para encerrar o programa.")
-else:
-    print("Nota inválida. Reiniciando questionário")
+while True:
     n1 = float(input("Digite a primeira nota do aluno: "))
     n2 = float(input("Digite a segunda nota do aluno: "))
+
     if n1 > 0 and n2 > 0:
-        print(f"As notas digitadas foram: {n1} e {n2}")
+        print(f"As notas digitadas foram: {n1} e {n2}, correto?")
         while True:
             s = input("Deseja calcular a média? (S/N) ")
             if s.upper() == "S":
@@ -31,4 +15,6 @@ else:
                 break
             else:
                 print("Opção inválida. Digite S para calcular a média ou N para encerrar o programa.")
+    else:
+        print("Nota inválida. Reiniciando questionário")
 
