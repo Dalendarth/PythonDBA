98bc12ac7de3e4b07b149af958f06f0efafefe6e
Guilherme <guilherme.scarpioni@live.com>
2023-03-19 12:20:12 -0300

    mPonderada

---
+++ Exercicios 2/MediaPonderada.py
@@ -0,0 +1,9 @@
+
+num1 = float(input("Digite a primeira nota"))
+num2 = float(input("Digite a segunda nota"))
+num3 = float(input("Digite a terceira nota"))
+p1 = 1
+p2 = 2
+mPonderada = (num1 * p1 + num2 * p2 + num3) / (p1 + p2 +1)
+
+print(f"A media ponderada da nota é: {mPonderada}")
