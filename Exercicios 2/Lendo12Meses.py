374ad8d64e9ac0ee5804c6b2cfe66682ec5303ad
Guilherme <guilherme.scarpioni@live.com>
2023-03-20 10:18:30 -0300

    EstruturaDecisaoSimples
    -----------------------
    
    BateriaExercicios
    
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios 2/Lendo12Meses.py
@@ -0,0 +1,30 @@
+
+
+Meses = int(input("Digite um numero:"))
+
+if Meses == 1:
+    print("Janeiro")
+elif Meses == 2:
+    print("Fevereiro")
+elif Meses == 3:
+    print("Março")
+elif Meses == 4:
+    print("Abril")
+elif Meses == 5:
+    print("Maio")
+elif Meses == 6:
+    print("Junho")
+elif Meses == 7:
+    print("Julho")
+elif Meses == 8:
+    print("Agosto")
+elif Meses == 9:
+    print("Setembro")
+elif Meses == 10:
+    print("Outubro")
+elif Meses == 11:
+    print("Novembro")
+elif Meses == 12:
+    print("Dezembro")
+else:
+    print("Numero Invalido")
