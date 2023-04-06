b555abe510066ab5aa58eec5cc151899cdc37bdf
Guilherme <guilherme.scarpioni@live.com>
2023-04-05 23:51:40 -0300

    EstruturaCondicional MasculinoFeminino.py -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios 2/MasculinoFeminino.py
@@ -0,0 +1,8 @@
+let = input('Digite uma das letras: F/M').lower()
+
+if let == 'f':
+    print('Sexo Feminino!')
+elif let == 'm':
+    print('Sexo Masculino!')
+else:
+    print('Entrada invalida!')
