83389f0f6bc68f664c86b260822f900290db7f6d
Guilherme <guilherme.scarpioni@live.com>
2023-04-08 12:17:20 -0300

    EstruturaCondicional ExerciosSecao3 -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

--- Exercicios 3/Lendo_10_Inteiros.py
+++ Exercicios 3/Lendo_10_Inteiros.py
@@ -2,13 +2,12 @@ num = []
 
 for i in range(1, 11):
     numero = int(input('Digite um número: '))
-    num.append(numero) # armazena 10 numeros na variavel num
+    num.append(numero)  # armazena 10 numeros na variavel num
 
 soma = sum(num)
 media = soma / len(num)
 
 print(f'A média dos números é: {media}')
-
 
 '''
 # Exemplo usando len
@@ -21,4 +20,3 @@ numeros = [1, 2, 3, 4, 5]
 soma = sum(numeros)
 print("A soma dos números é:", soma)
 '''
-
