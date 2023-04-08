247ad9bd12664d222ecd05a91ced97d1fd33dd2f
Guilherme <guilherme.scarpioni@live.com>
2023-04-07 22:38:51 -0300

    EstruturaCondicional ExerciosSecao3 -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios 3/ExercicioRepeticao.py
@@ -0,0 +1,4 @@
+for n in range(3):
+    for c in range(1, 101, 1):
+        print(c)
+
---
+++ Exercicios 3/MultiploDe3.py
@@ -0,0 +1,4 @@
+for n in range(1, 11):
+    if n % 3 == 0:  # se resultado da divisao inteira for 0
+        print(n)
+
