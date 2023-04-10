97240302659809c4f1e308ce8ae4657fe8436577
Guilherme <guilherme.scarpioni@live.com>
2023-04-10 00:01:44 -0300

    EstruturaCondicional exercicios_loop_for -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios1/nome_usuario.py
@@ -0,0 +1,13 @@
+nome = input('Qual é o seu nome?')
+print(f'Olá {nome}! Prazer em te conhecer!')
+
+dia = int(input('Dia: '))
+mes = input('Mes: ')
+ano = int(input('Ano:'))
+
+print(f'Você nasceu no dia {dia} de {mes} de {ano}. Correto?')
+
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero:'))
+soma = n1 + n2
+print(f'O resultado da soma é: {soma}')
