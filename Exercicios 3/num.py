19cf30c508c73862348b53b59adf695223371b6a
Guilherme <guilherme.scarpioni@live.com>
2023-04-13 13:57:23 -0300

    EstruturaCondicional teste_while -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios 3/num.py
@@ -0,0 +1,12 @@
+num = 0
+while num < 1000000000:
+    num += 1
+    print(num)
+
+# Este código cria uma variável chamada "num" e a inicializa com o valor 0. Em seguida, ele entra em um loop "while",
+# que continua a ser executado enquanto o valor de "num" for menor que 1000000000.
+
+# Dentro do loop, o valor de "num" é incrementado em 1 unidade a cada iteração usando o operador de adição (+=),
+# e o valor atualizado é impresso na tela usando a função "print()".
+
+# Este código imprime os números de 1 a 1000000000, um por linha.
