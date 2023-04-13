4cd7118ff88bfd9c3ebbc4e122b89d0eeab456dc
Guilherme <guilherme.scarpioni@live.com>
2023-04-13 11:16:52 -0300

    EstruturaCondicional cadastro_cliente -----------------------
    Venda
    -----------------------
    
    ParaMemorizaçãoLongoPrazo

---
+++ Exercicios 3/cadastro_cliente.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+from flask import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cadastro.db'
+db = SQLAlchemy(app)
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    senha = db.Column(db.String(50))
+
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    endereco = db.Column(db.String(50))
+
+
+@app.route('/usuarios', methods=['GET'])
+def get_usuarios():
+    usuarios = Usuario.query.all()
+    return jsonify([u.__dict__ for u in usuarios])
+
+
+@app.route('/usuarios', methods=['POST'])
+def create_usuario():
+    data = request.get_json()
+    novo_usuario = Usuario(nome=data['nome'], email=data['email'], senha=data['senha'])
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return jsonify(novo_usuario.__dict__)
+
+
+@app.route('/clientes', methods=['GET'])
+def get_clientes():
+    clientes = Cliente.query.all()
+    return jsonify([c.__dict__ for c in clientes])
+
+
+@app.route('/clientes', methods=['POST'])
+def create_cliente():
+    data = request.get_json()
+    novo_cliente = Cliente(nome=data['nome'], email=data['email'], endereco=data['endereco'])
+    db.session.add(novo_cliente)
+    db.session.commit()
+    return jsonify(novo_cliente.__dict__)
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
